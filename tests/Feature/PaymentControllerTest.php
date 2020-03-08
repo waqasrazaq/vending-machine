96521e02430e8ef050523beefc31832759792eb0
Waqas <waqas@android-8b2a64bd556f0e0e.localdomain>
2020-03-08 23:43:35 +0100

    Adding feature tests for Payment Controller

---
+++ tests/Feature/PaymentControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+use App\Infrastructure\Repositories\RedisMemoryRepository;
+use Illuminate\Http\JsonResponse;
+
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PaymentControllerTest extends TestCase
+{
+    protected function setUp():void
+    {
+        parent::setUp();
+    }
+
+    public function testInsertMoney()
+    {
+        $response = $this->json('post', '/api/payments', ["coin"=>0.05]);
+        $response->assertStatus(200);
+        $response->assertJsonFragment(["results"=>true]);
+    }
+
+    public function testInsertMoneyWithInvalidCoins()
+    {
+        $response = $this->json('post', '/api/payments', ["coin"=>76]);
+        $response->assertStatus(JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonFragment([config('common.error_payment_coin_value')]);
+    }
+
+    public function testGetAddedMoney()
+    {
+        $this->json('post', '/api/payments', ["coin"=>0.05]);
+        $this->json('post', '/api/payments', ["coin"=>0.05]);
+        $this->json('post', '/api/payments', ["coin"=>0.05]);
+
+        $response = $this->json('get', '/api/payments');
+        $response->assertStatus(JsonResponse::HTTP_OK);
+        $this->assertGreaterThanOrEqual(3, sizeof($response["results"]));
+    }
+
+    public function tearDown():void
+    {
+        $redis = new RedisMemoryRepository();
+        $redis->removeItemsFromList(config('common.inserted_coins_key'), [0.05,0.05,0.05,0.05]);
+        parent::tearDown();
+    }
+}
